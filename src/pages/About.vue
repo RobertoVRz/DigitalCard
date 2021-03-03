f74110c968722bb7161f406e534841f701b88daf
RobertoVrz <roberto.vzrz@gmail.com>
2021-03-03 03:21:29 -0600

    QR added

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,7 @@
 <template>
     <Layout>
         <div class="flex flex-col justify-center items-center">
-            <g-image class="mt-40" alt="Example image" src="~/favicon.png" width="135" />
+            <g-image class="mt-40" alt="Example image" src="~/assets/img/qr-code.svg" width="235" />
             <h1 class="mt-5">Nombre de Empresa</h1>
         </div>
     </Layout>
